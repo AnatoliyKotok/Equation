26d760e9d91926fb32389ed061d18d271276f851
AnatoliyKotok <anatolijkotok@gmail.com>
2020-10-01 16:36:58 +0300

    first commit

---
+++ equation/Equation.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace equation_
+{
+    class LEquation
+    {
+        int a;
+
+
+
+        public int Root { get => -B / a; }
+        public int A
+        {
+            get => a;
+
+
+            set
+            {
+                if (value == 0)
+                {
+
+                    throw new Exception("a is a bad value");
+                }
+                else
+                {
+                    a = value;
+                }
+            }
+        }
+
+        public int B { get; set; }
+
+        public static LEquation Parse(string str)
+        {
+
+            string[] arrStr = str.Split(" \t".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+            if (arrStr.Length != 2)
+            {
+                throw new Exception("bad quantuti coef");
+            }
+
+            if (!int.TryParse(arrStr[0], out int a))
+            {
+                throw new Exception("Not parse first coef");
+            }
+            if (!int.TryParse(arrStr[1], out int b))
+            {
+                throw new Exception("Not parse second coef");
+            }
+
+            return new LEquation(a, b);
+
+        }
+        public LEquation(int a, int b)
+        {
+            this.A = a;
+            this.B = b;
+
+        }
+
+        public override string ToString()
+        {
+            return $"Coeficient A {A} : Coeficient : {B} ";
+        }
+
+    }
+}
+
---
+++ equation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace equation_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                LEquation le = new LEquation(4, 0);
+                Console.WriteLine(le.Root);
+                LEquation le2 = LEquation.Parse("3 6");
+                Console.WriteLine(le2.Root);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
